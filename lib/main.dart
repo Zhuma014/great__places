067b39ea2738a36e00ca8eb4765042bf50a590d0
Zhumabek Sabit <zhumabeksabit@gmail.com>
2023-03-28 08:21:54 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/great_places_provider.dart';
+import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
+import './screens/place_details.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          PlaceDetailsScreen.routeName: (ctx) => PlaceDetailsScreen(),
+        },
+        ),
+    );
+  }
+}
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/map_screen.dart';
+import 'package:location/location.dart';
+import '../helpers/location_helper.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LocationInput extends StatefulWidget {
+  final Function onSelectPlace;
+  LocationInput(this.onSelectPlace);
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+  void _showPreview(double lat, double lng) {
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: lat, longitude: lng);
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    _showPreview(locData.latitude!, locData.longitude!);
+    widget.onSelectPlace(locData.latitude, locData.longitude);
+    print(locData.latitude!);
+    print(locData.longitude!);
+  }
+
+  Future<void> _selectOnMap() async {
+    final LatLng selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+
+    if (selectedLocation == null) {
+      return;
+    }
+    _showPreview(selectedLocation.latitude, selectedLocation.longitude);
+    widget.onSelectPlace(selectedLocation.latitude, selectedLocation.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl == null
+              ? Text(
+                  'No Location Chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(_previewImageUrl!,
+                  fit: BoxFit.cover, width: double.infinity),
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          TextButton.icon(
+            onPressed: _getCurrentUserLocation,
+            icon: Icon(
+              Icons.location_on,
+            ),
+            label: Text('Current Location'),
+            style: TextButton.styleFrom(
+                foregroundColor: Colors.blueAccent), // Text Color
+          ),
+          TextButton.icon(
+            onPressed: _selectOnMap,
+            icon: Icon(
+              Icons.map,
+            ),
+            label: Text('Select on Map'),
+            style: TextButton.styleFrom(
+                foregroundColor: Colors.blueAccent), // Text Color
+          ),
+        ]),
+      ],
+    );
+  }
+}
